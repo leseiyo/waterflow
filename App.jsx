eae1bc0c9eb4357cab7bdc044f1108551f0962f9
leseiyo <artbyseiyojnr@gmail.com>
2025-09-14 01:11:26 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-50">
+      <h1 className="text-4xl font-bold text-blue-600">WaterFlow - Coming Soon</h1>
+    </div>
+  );
+}
+
+export default App;
